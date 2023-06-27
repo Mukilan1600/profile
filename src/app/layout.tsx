91ae0e52e473aeb5cf1cdaefb6faf070caac9034
Mukilan I K <ikmukila@amazon.com>
2023-06-27 14:32:27 +0530

    Fixed metadata client component and resized image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
-"use client";
-import { AnimatePresence } from "framer-motion";
+
 import Nav from "./components/Nav";
 import "./globals.css";
 import { Quicksand } from "next/font/google";
-import React, { ReactElement } from "react";
-import { usePathname } from "next/navigation";
+import React from "react";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 
@@ -22,9 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={quicksand.className}>
         <Nav>
-          <AnimatePresence mode="wait">
             {children}
-          </AnimatePresence>
         </Nav>
       </body>
     </html>
